4f461f72150e3b7651266f563c3334099036ea18
muhamsh <muhammad-ashraful@outlook.com>
2025-04-21 15:53:26 +0600

    splitting the code

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-const http = require( 'http' );
-const { handleReqRes } = require( "./utils/handleReqRes" );
-const { sendTwilioSms } = require( './utils/notifications' );
+// const http = require( 'http' );
+// const { handleReqRes } = require( "./utils/handleReqRes" );
+// const { sendTwilioSms } = require( './utils/notifications' );
+
+const server = require( './utils/server' );
+const worker = require( './utils/worker' );
 
 const app = {};
 
-sendTwilioSms();
+// sendTwilioSms();
 
 // configuration
-app.config = {
-    port: 3000,
-};
+// app.config = {
+//     port: 3000,
+// };
 
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
-    });
-};
+// // create server
+// app.createServer = () => {
+//     const server = http.createServer(app.handleReqRes);
+//     server.listen(app.config.port, () => {
+//         console.log(`listening to port ${app.config.port}`);
+//     });
+// };
 
-// handle Request Response
-app.handleReqRes = handleReqRes;
+// // handle Request Response
+// app.handleReqRes = handleReqRes;
 
-// start the server
-app.createServer();
+// // start the server
+// app.createServer();
+
+app.init()=> {
+    // start the server
+    server.createServer();
+    // start the workers
+    worker.init();
+}
+
+app.init();
+
+module.exports = app;
---
+++ utils/server.js
@@ -0,0 +1,34 @@
+const http = require( 'http' );
+const { handleReqRes } = require( "./handleReqRes" );
+const { sendTwilioSms } = require( './notifications' );
+
+
+const server = {};
+
+sendTwilioSms();
+
+// configuration
+server.config = {
+    port: 3000,
+};
+
+// create server
+server.createServer = () => {
+    const serverInit = http.createServer(server.handleReqRes);
+    serverInit.listen(server.config.port, () => {
+        console.log(`listening to port ${server.config.port}`); 
+    });
+};
+
+// handle Request Response
+server.handleReqRes = handleReqRes;
+
+// start the server
+// server.createServer();
+server.init = () =>
+{
+    // start the server
+    server.createServer();
+};
+
+module.exports = server;
